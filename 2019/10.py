c82e9da0249ae671e629c8efbf6bbc42070c4ca0
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2021-08-24 21:12:24 -0400

    Solve part 2
    Finally jesus christ

--- 2019/10.py
+++ 2019/10.py
@@ -1,8 +1,9 @@
 import math
+from itertools import cycle
 
 
 with open("input.txt") as fin:
-    L = fin.read().splitlines()
+    L = [list(s) for s in fin.read().splitlines()]
 
 
 def p1():
@@ -11,9 +12,58 @@ def p1():
         for j in range(len(L[0])):
             if L[i][j] == "#":
                 a = visible(i, j)
+                if a == 261:
+                    print(j, i)
                 if a > m:
                     m = a
     return m - 1
+
+
+def p2():
+    x, y = 14, 17
+    points = []
+    for i in range(len(L)):
+        for j in range(len(L[0])):
+            if L[i][j] == "#":
+                points.append((j, i))
+
+    points.sort(
+        key=lambda coords: (
+            coords[0] - x < 0 if coords[0] - x != 0 else y < 0,
+            (float("inf") if coords[1] > y else float("-inf"), -abs(coords[1] - y))
+            if coords[0] - x == 0
+            else (
+                (coords[1] - y) / (coords[0] - x),
+                (abs(coords[1] - y) + abs(coords[0] - x)),
+            ),
+        )
+    )
+
+    gone = [None]
+    v = set()
+    cur = 0
+    testing = []
+    for p in cycle(points):
+        if (
+            gone[-1]
+            != (
+                prev := (float("inf") if p[1] > y else float("-inf"))
+                if p[0] - x == 0
+                else (p[1] - y) / (p[0] - x)
+            )
+            and p not in v
+        ):
+            gone.append(prev)
+            cur += 1
+            testing.append((p, round(prev, 2)))
+            if cur > 300:
+                return testing
+            v.add(p)
+            if cur == 200:
+                print(testing)
+                print(p)
+                return p[0] * 100 + p[1]
+
 
 def visible(i, j):
     a = 0
@@ -36,4 +86,5 @@ def visible(i, j):
                     a += 1
     return a
 
-print(p1())
+
+print(p2())
