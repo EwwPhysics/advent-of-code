57dca35ccfaebc01cc88b8a8dd2df8b7080c1bd0
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2021-12-01 16:01:31 -0500

    Solve 2021 day 1

---
+++ 2021/01.py
@@ -0,0 +1,13 @@
+with open("input.txt") as f:
+    data = list(map(int, f.read().splitlines()))
+
+def p1(data):
+    return sum(b > a for a, b in zip(data, data[1:]))
+
+
+def p2(data):
+    return sum(b > a for a, b in zip(data, data[3:]))
+
+
+print(p1(data))
+print(p2(data))
