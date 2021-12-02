6ea0ed1e2e87886d83f76ede96e8c00204ebbafd
Sasha <74149971+EwwPhysics@users.noreply.github.com>
2021-12-02 12:26:56 -0500

    Solve day 2 puzzles

---
+++ 2021/02.py
@@ -0,0 +1,37 @@
+with open("input.txt") as fin:
+    data = [x.split() for x in fin.read().splitlines()]
+
+
+def p1(file):
+    f = d = 0
+    for a, b in file:
+        b = int(b)
+        if a == "forward":
+            f += b
+        elif a == "up":
+            d -= b
+        else:
+            d += b
+
+    return f * d
+
+
+def p2(file):
+    f = d = aim = 0
+    
+    for a, b in file:
+        b = int(b)
+
+        if a == "forward":
+            f += b
+            d += aim * b
+        elif a == "down":
+            aim += b
+        else:
+            aim -= b
+
+    return f * d
+
+
+print(p1(data))
+print(p2(data))
